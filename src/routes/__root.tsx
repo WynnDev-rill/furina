d4ba1eabea66cc4dcbf52fe3b8d0d8063134dcec
Wynn Dev <indonesiafilmku@gmail.com>
2026-08-02 02:59:27 +0700

    Load unified offline cache and Japanese voice controls

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -126,6 +126,8 @@ function RootShell({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
+        <script src="/furina-sw-register.js" defer />
+        <script src="/furina-voice.js" defer />
         <Scripts />
       </body>
     </html>
